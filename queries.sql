6eb5451b0d85e0320ca79074622ac33cdbc22d63
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-14 14:56:20 +0100

    query select one

--- queries.sql
+++ queries.sql
@@ -1,8 +1,10 @@
 -- ---------------------------------------  QUERY CON SELECT
-​
+
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-​
-​
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE YEAR(`date_of_birth`) = 1990;
+
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 ​
 ​
