c0280393e358e16bea719ceaa84e0616489be79b
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-14 15:41:57 +0100

    query group one

--- queries.sql
+++ queries.sql
@@ -40,11 +40,12 @@ FROM `teachers`
 WHERE `phone` IS NULL;
 
 -- ------------------------------------------ QUERY CON GROUP BY
-​
-​
+
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-​
-​
+SELECT COUNT(*) AS 'Enrolled students', YEAR(`enrolment_date`) 
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 ​
 ​
