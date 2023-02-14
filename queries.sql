a63d44246e72b6a15d6ce315419c6df1278b6469
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-14 17:57:11 +0100

    add date function curdate to query select three

--- queries.sql
+++ queries.sql
@@ -13,7 +13,7 @@ WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE `date_of_birth` > '1993';
+WHERE YEAR(CURDATE()) - YEAR(`date_of_birth`) > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT `name`, `period`, `year`
