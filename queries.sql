ccf5aa0de702fb01cef79adb02c90e1b484af73c
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-15 10:45:59 +0100

    corrections

--- queries.sql
+++ queries.sql
@@ -13,7 +13,7 @@ WHERE `cfu` > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
-WHERE YEAR(CURDATE()) - YEAR(`date_of_birth`) > 30;
+WHERE `date_of_birth` < DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT `name`, `period`, `year`
@@ -23,7 +23,7 @@ WHERE `period` = 'I semestre' AND `year` = '1';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT `course_id`, `date`, `hour`
 FROM `exams`
-WHERE `date` = '2020-06-20' AND `hour` > '14:%';
+WHERE `date` = '2020-06-20' AND HOUR(`hour`) > 14;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT `name`, `level`
