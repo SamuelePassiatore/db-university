21792b19e9ece40ec0c3444ce919cdae4f3134e4
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-15 15:49:29 +0100

    queries join six

--- queries_join.sql
+++ queries_join.sql
@@ -46,7 +46,17 @@ ON `teachers`.`id` = `course_teacher`.`teacher_id`
 ORDER BY `degrees`.`name`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 
-
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, --`departments`.`name` AS 'Department'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees` 
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 -- BONUS. Selezionare per ogni studente quanti tentativi d'esame ha sostenuto per superare ciascuno dei suoi esami
 
